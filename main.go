f99a2080d74bcaf06ee993c8799faf6512d38245
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-01-27 19:59:22 +0300

    Add details to the plan

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 func main() {
-	// 1. enter data
-	// 2. process data
+	// 1.1 enter data ("2d20")
+	// 1.2 parse entered data (move to a separate function)
+	// 2.1 process data: generate several random numbers (rand.Intn(20)+1 in loop) (func)
+	// 2.2 process data: calculate min, max and sum (func)
 	// 3. output data
 }
