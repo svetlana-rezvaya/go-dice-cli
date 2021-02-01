5c36d7880c4b57bcf6afce481f2d9dcb4714b861
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-02-01 21:16:12 +0300

    Complete generateDiceThrows function

--- generate_dice_throws.go
+++ generate_dice_throws.go
@@ -1,5 +1,13 @@
 package main
 
-func generateDiceThrows(throwCount int, faceCount int) []int {
+import "math/rand"
 
+func generateDiceThrows(throwCount int, faceCount int) []int {
+	diceThrows := []int{}
+	for i := 0; i < throwCount; i = i + 1 {
+		value := rand.Intn(faceCount) + 1
+		diceThrows = append(diceThrows, value)
+	}
+
+	return diceThrows
 }
