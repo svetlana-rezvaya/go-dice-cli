d767ff2bd6a12f539ee68242b93293aa85fa115d
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-02-09 21:19:36 +0300

    Enhance parseDiceNotation function

--- parse_dice_notation.go
+++ parse_dice_notation.go
@@ -6,16 +6,20 @@ import (
 	"strings"
 )
 
-// example of input data: "2d20"
+// example of input data: "2d20", "d20" (it means "1d20")
 func parseDiceNotation(text string) (throwCount int, faceCount int, err error) {
 	parts := strings.Split(text, "d")
 	if len(parts) != 2 {
 		return 0, 0, errors.New("invalid count of parts")
 	}
 
-	throwCount, err = strconv.Atoi(parts[0])
-	if err != nil {
-		return 0, 0, errors.New("unable to parse the throw count: " + err.Error())
+	if parts[0] == "" {
+		throwCount = 1
+	} else {
+		throwCount, err = strconv.Atoi(parts[0])
+		if err != nil {
+			return 0, 0, errors.New("unable to parse the throw count: " + err.Error())
+		}
 	}
 
 	faceCount, err = strconv.Atoi(parts[1])
