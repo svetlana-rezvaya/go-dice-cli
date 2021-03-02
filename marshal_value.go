85b3e41c6d1110ea87dc15a946f98e9cf54426e0
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-02 22:34:04 +0300

    Add the second case of "toUnicodeDices" function

--- marshal_value.go
+++ marshal_value.go
@@ -4,6 +4,9 @@ func toUnicodeDices(value int) string {
 	if value <= 6 {
 		return string('\u2680' + value - 1)
 	}
+	if value < 10 {
+		return "\u2685" + toUnicodeDices(value-6)
+	}
 
 	panic("toUnicodeDices: not yet implemented")
 }
--- marshal_value_test.go
+++ marshal_value_test.go
@@ -17,6 +17,9 @@ func Test_toUnicodeDices(test *testing.T) {
 		data{value: 4, wantedResult: "⚃"},
 		data{value: 5, wantedResult: "⚄"},
 		data{value: 6, wantedResult: "⚅"},
+		data{value: 7, wantedResult: "⚅⚀"},
+		data{value: 8, wantedResult: "⚅⚁"},
+		data{value: 9, wantedResult: "⚅⚂"},
 	}
 	for _, testData := range tests {
 		result := toUnicodeDices(testData.value)
