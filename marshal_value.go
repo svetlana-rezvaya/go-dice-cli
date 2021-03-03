7fc5e3f8a217232214d07518ca8ba832da569a6f
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-03 21:07:54 +0300

    Add the third case of "toUnicodeDices" function

--- marshal_value.go
+++ marshal_value.go
@@ -1,6 +1,11 @@
 package main
 
+import "strings"
+
 func toUnicodeDices(value int) string {
+	if value == 0 {
+		return ""
+	}
 	if value <= 6 {
 		return string('\u2680' + value - 1)
 	}
@@ -8,5 +13,8 @@ func toUnicodeDices(value int) string {
 		return "\u2685" + toUnicodeDices(value-6)
 	}
 
-	panic("toUnicodeDices: not yet implemented")
+	order := value / 10
+	rest := value % 10
+	result := strings.Repeat("\u2684\u2684 ", order) + toUnicodeDices(rest)
+	return strings.TrimSpace(result)
 }
