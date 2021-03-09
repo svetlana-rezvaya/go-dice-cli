6415957432dc84f2277954510b817bcb8382667e
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-09 21:02:32 +0300

    Enhance marshalValues function

--- marshal_values.go
+++ marshal_values.go
@@ -1,15 +1,13 @@
 package main
 
-import "strconv"
-
 // for []int{1, 2, 3}, this function returns "1, 2, 3"
-func marshalValues(values []int) string {
+func marshalValues(values []int, useUnicode bool) string {
 	result := ""
 	for _, value := range values {
 		if result != "" {
 			result = result + ", "
 		}
-		result = result + strconv.Itoa(value)
+		result = result + marshalValue(value, useUnicode)
 	}
 
 	return result
--- marshal_values_test.go
+++ marshal_values_test.go
@@ -2,10 +2,18 @@ package main
 
 import "testing"
 
-func Test_marshalValues(test *testing.T) {
-	result := marshalValues([]int{1, 2, 3})
+func Test_marshalValues_withoutUnicode(test *testing.T) {
+	result := marshalValues([]int{1, 2, 3}, false)
 
 	if result != "1, 2, 3" {
+		test.Fail()
+	}
+}
+
+func Test_marshalValues_withUnicode(test *testing.T) {
+	result := marshalValues([]int{1, 2, 3}, true)
+
+	if result != "⚀, ⚁, ⚂" {
 		test.Fail()
 	}
 }
