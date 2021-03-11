2a7ee9e8998a5a0f792e0f230b633eb541feb864
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-11 21:27:00 +0300

    Implement main function

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
 func main() {
-	// 1.1 enter data ("2d20")
-	// 1.2 parse entered data (move to a separate function)
-	// 2.1 process data: generate several random numbers (rand.Intn(20)+1 in loop) (func)
-	// 2.2 process data: calculate min, max and sum (func)
-	// 3. output data
+	rand.Seed(time.Now().UnixNano())
+
+	options, err := parseFlags(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	diceThrows := generateDiceThrows(options.throwCount, options.faceCount)
+	fmt.Println(marshalValues(diceThrows, options.useUnicode))
+
+	stats := collectStatistics(diceThrows)
+	fmt.Print(marshalStatistics(stats, options.useUnicode))
 }
