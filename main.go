f44fffd025b0b133d3919f46d84c207344d79abf
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-15 21:16:52 +0300

    Fix processing of "help" flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
@@ -8,11 +9,32 @@ import (
 	"time"
 )
 
+const helpMessage = "" +
+	"Usage:\n" +
+	"  go-dice-cli -h | -help | --help\n" +
+	"  go-dice-cli [-unicode] <dice>\n" +
+	"  go-dice-cli [-unicode] -dice STRING\n" +
+	"  go-dice-cli [-unicode] -throws INTEGER -faces INTEGER\n" +
+	"\n" +
+	"Options:\n" +
+	"  -h, -help, --help  — show the help message and exit;\n" +
+	"  -dice STRING       — number of throws and dice faces in the dice notation;\n" +
+	"  -throws INTEGER    — number of throws;\n" +
+	"  -faces INTEGER     — number of dice faces;\n" +
+	"  -unicode           — use Unicode to show dices.\n" +
+	"\n" +
+	"Arguments:\n" +
+	"  <dice>  — number of throws and dice faces in the dice notation.\n"
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	options, err := parseFlags(os.Args[1:])
 	if err != nil {
+		if err == flag.ErrHelp {
+			fmt.Print(helpMessage)
+			os.Exit(0)
+		}
 		log.Fatal(err)
 	}
 
