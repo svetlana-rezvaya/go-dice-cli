f25efb0aa4558fa434e772f0dfe7f2a9f02dc3c7
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-09-27 21:44:39 +0300

    Move the helpMessage constant

--- parse_flags.go
+++ parse_flags.go
@@ -7,6 +7,23 @@ import (
 	"io/ioutil"
 )
 
+const helpMessage = "" +
+	"Usage:\n" +
+	"  go-dice-cli -h | -help | --help\n" +
+	"  go-dice-cli [-unicode] <dice>\n" +
+	"  go-dice-cli [-unicode] -dice STRING\n" +
+	"  go-dice-cli [-unicode] -throws INTEGER -faces INTEGER\n" +
+	"\n" +
+	"Options:\n" +
+	"  -h, -help, --help  — show the help message and exit;\n" +
+	"  -dice STRING       — number of throws and dice faces in the dice notation;\n" +
+	"  -throws INTEGER    — number of throws;\n" +
+	"  -faces INTEGER     — number of dice faces;\n" +
+	"  -unicode           — use Unicode to show dices.\n" +
+	"\n" +
+	"Arguments:\n" +
+	"  <dice>  — number of throws and dice faces in the dice notation.\n"
+
 type options struct {
 	throwCount int
 	faceCount  int
